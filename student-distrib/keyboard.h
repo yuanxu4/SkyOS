92084b434e07ba468823b1b78580e48a51b0e07a
Lou Haina <hainalou89@gmail.com>
2022-03-21 21:07:16 -0500

    add comments

--- student-distrib/keyboard.h
+++ student-distrib/keyboard.h
@@ -7,13 +7,6 @@
 #include "types.h"
 #define KEYBOARD_PORT   0x60
 #define KEYBARD_IRQ     0x01
-#define SIMPLE_CASE     0x35
+#define SIMPLE_CASE     0x36
 #define ENTER           0X1c
 
-const char scancode_simple_lowcase[SIMPLE_CASE]{                
-    0,0,'1','2','3','4','5','6','7','8',    // 0x00-0x09
-    '9','0','-','=',0,0,'q','w','e','r','t',// 0x0a-0x14
-    'y','u','i','o','p','[',']',0,0,'a','s',
-    'd','f','g','h','j','k','l',';',0,0,0,0,
-    'z','x','c','v','b','n','m',',','.','/'     // 0x2c-0x35   
-}
