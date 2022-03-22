b0b1b4a87ec1cdee5d4ab1b61a90387fd9ed3204
Lou Haina <hainalou89@gmail.com>
2022-03-21 21:13:58 -0500

    add comments

--- student-distrib/i8259.c
+++ student-distrib/i8259.c
@@ -16,12 +16,15 @@ uint8_t slave_mask;  /* IRQs 8-15 */
  * Side Effects: init i8259
  *  first mask all interrupts then use four 
  * initialization control words
+ * https://wiki.osdev.org/8259_PIC
  */
 void i8259_init(void) {
-    
+    /* init two masks */
+    master_mask = 0xFF;
+    slave_mask = 0xFF;
 
     /* SPIN_LOCK_IRQSAVE???? */
-
+    cli();
     /* mask all of PICs */
     outb(0XFF, MASTER_8259_DATA);   // 0xff mask all bits of interrupt
     outb(0xFF, SLAVE_8259_DATA);
@@ -39,6 +42,7 @@ void i8259_init(void) {
     /* restore mask of all IRQ */
     outb(master_mask, MASTER_8259_DATA);
     outb(slave_mask, SLAVE_8259_DATA);
+    sti();
 }
 
 /* Enable (unmask) the specified IRQ */
@@ -46,19 +50,21 @@ void i8259_init(void) {
  * Inputs: irq_num -- interrupt request number
  * Outputs: None
  * Side Effects: unmask specified IRQ
- * 
+ * https://wiki.osdev.org/8259_PIC
  */
 void enable_irq(uint32_t irq_num) {
     uint8_t value;
     uint8_t value_master;
+    /* less than 8 means irq of master PIC */
     if (irq_num < 8){
-        value = inb(MASTER_8259_DATA)&(~(1<<irq_num));
+        value = inb(MASTER_8259_DATA)&(~(0x01<<irq_num)); //0x01 bit mask, active low
         outb(value, MASTER_8259_DATA);
     }else{
+        /* get corresponding pin number */
         irq_num-=8;
-        value = inb(SLAVE_8259_DATA)&(~(1<<irq_num));
+        value = inb(SLAVE_8259_DATA)&(~(0x01<<irq_num)); //0x01 bit mask
         outb(value, SLAVE_8259_DATA);
-        /* unmask the IR2 in master PIC*/
+        /* unmask the IR2(0100) in master PIC*/
         value_master = inb(MASTER_8259_DATA)&(~(0x04));
         outb(value_master, MASTER_8259_DATA);
     }
@@ -69,19 +75,19 @@ void enable_irq(uint32_t irq_num) {
  * Inputs: irq_num -- interrupt request number
  * Outputs: None
  * Side Effects: mask specified IRQ
- * 
+ * https://wiki.osdev.org/8259_PIC
  */
 void disable_irq(uint32_t irq_num) {
     uint8_t value;
     uint8_t value_master;
     /* irq of Master PIC*/
     if (irq_num < 8){
-        value = inb(MASTER_8259_DATA)|(0x01<<irq_num);
+        value = inb(MASTER_8259_DATA)|(0x01<<irq_num);//0x01 bit mask
         outb(value, MASTER_8259_DATA);
     }else{
         /* irq of Slave PIC */
         irq_num-=8;
-        value = inb(SLAVE_8259_DATA)|(0x01<<irq_num);
+        value = inb(SLAVE_8259_DATA)|(0x01<<irq_num);//0x01 bit mask
         outb(value, SLAVE_8259_DATA);
         /* if all slave PIC masked mask master's IR2 */
         if (value == 0xFF){
@@ -102,11 +108,12 @@ void disable_irq(uint32_t irq_num) {
 void send_eoi(uint32_t irq_num) {
     uint8_t value;
     uint8_t value_master;
+    /* irq of Slave PIC*/
     if (irq_num >= 8){
         /* irq of Slave PIC */
         irq_num-=8;
         value = irq_num | EOI;
-        /* send EOI to master PIC */
+        /* send EOI to master PIC and also mask slave port */
         value_master = 2 | EOI;
         outb(value,SLAVE_8259_COMMAND);
         outb(value_master, MASTER_8259_COMMAND);
