5c9f58680d1f8a1d79575e0146c99ed6694ca7e9
Youch <Youcheng.19@intl.zju.edu.cn>
2022-04-10 00:45:45 -0500

    syscall fix bugs

--- student-distrib/tests.c
+++ student-distrib/tests.c
@@ -568,6 +568,6 @@ void launch_tests()
 	// TEST_OUTPUT("idt_dereference_test", idt_dereference_test());
 	// TEST_OUTPUT("Keyboard_test", keyboard_test());
 	// TEST_OUTPUT("pic_garbage_test", pic_garbage_test());
-	TEST_OUTPUT("file_sys_test", file_sys_test());
+	// TEST_OUTPUT("file_sys_test", file_sys_test());
 	//halt(1);
 }
